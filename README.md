10fa9cd4884c33ca34b41096b1c2dfa5fe0b5ade
vyeung7 <Starloop2003@gmail.com>
2024-02-02 13:44:04 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ex.3
 exercise 3 creating a rep from scratch 
+
+"When you're in your darkest place, you give yourself hope and that's inner strength." -Uncle Iroh
---
+++ index.js
@@ -0,0 +1,6 @@
+let qoutes =[
+    "Anyone who has never made a mistake has never tried anything new. - Albert Einstein",
+    "If you don't like your destiny, don't accept it. Instead, have the courage to change it the way you want it to be. - Naruto Uzumaki",
+    "People, who can not throw something important away, can never hope to change anything. - Armin Arlert"
+]
+console.log(qoutes[Math.floor(Math.random()*3)])
